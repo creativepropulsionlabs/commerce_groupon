d8a9c357d0e42fe733b600c5a05f6e6dd027a905
Oleg Natalushko <oleg.natalushko@webanywhere.co.uk>
2019-09-12 16:04:24 +0300

    mapper modifications

--- src/Plugin/CommerceGrouponShippingMapper/Best.php
+++ src/Plugin/CommerceGrouponShippingMapper/Best.php
@@ -15,10 +15,17 @@ use Drupal\commerce_shipping\Entity\Shipment;
  *   label = @Translation("Best"),
  *   groupon_shipping_method = "BEST",
  *   drupal_shipping_method_plugin = "flat_rate",
- *   weight = 1000
+ *   weight = 1
  * )
  */
 
 class Best extends CommerceGrouponShippingMapperPluginPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCarrier(OrderInterface $order) {
+    return 'UPS';
+  }
 
 }
